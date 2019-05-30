e87ab1b31360a3cae98985d7d918a4116dcb10e8
az <azizkoura@hotmail.com>
2019-05-30 16:59:42 +0200

    Using Template URLs

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@
                 </div>
                 <button class="btn btn-primary" @click="submit">submit</button>
                 <hr>
+                <input type="text" class="form-control" v-model="node">
+                <br><br>
                 <button class="btn btn-success" @click="fetchData">Get Data</button>
                 <br><br>
                 <ul class="list-group">
@@ -34,7 +36,8 @@
             email: ''
           },
           users: [],
-          resource:{}
+          resource:{},
+          node: 'data'
         }
       },
       methods: {
@@ -49,23 +52,34 @@
           this.resource.saveAlt(this.user)
         },
         fetchData() {
-          this.$http.get('data.json')
-              .then(response => {
-                return response.json()
-              }).then(data => {
-                const resultArray = []
-                for(let key in data){
-                  resultArray.push(data[key])
-                }
-                this.users = resultArray
-              })    
+          // this.$http.get('data.json')
+          //     .then(response => {
+          //       return response.json()
+          //     }).then(data => {
+          //       const resultArray = []
+          //       for(let key in data){
+          //         resultArray.push(data[key])
+          //       }
+          //       this.users = resultArray
+          //     })    
+          this.resource.getData({node: this.node})
+                 .then(response => {
+                    return response.json()
+                  }).then(data => {
+                  const resultArray = []
+                  for(let key in data){
+                      resultArray.push(data[key])
+                  }
+                  this.users = resultArray
+               })        
         }
       },
       created() {
         const customActions = {
-          saveAlt:{method:'POST', url: 'alternative.json'}
+          saveAlt:{method:'POST', url: 'alternative.json'},
+          getData: {method:'GET'}
         }
-        this.resource = this.$resource('data.json',{}, customActions)
+        this.resource = this.$resource('{node}.json',{}, customActions)
       }
     }
 </script>
