d20dfbec47968b3fe84fe27aa2d577f551b4b212
az <azizkoura@hotmail.com>
2019-05-30 15:58:28 +0200

    Intercepting Request

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App.vue'
+import { request } from 'https';
 
 Vue.config.productionTip = false
 
 Vue.use(VueResource)
 
 Vue.http.options.root = 'https://vue-resource-fef4c.firebaseio.com/data.json'
+Vue.http.interceptors.push((request, next) =>{
+  console.log(request)
+  if(request.method == 'POST'){
+    request.method = 'PUT'
+  }
+  next((response) => {
+    response.json = () => { 
+      return { 
+        message: response.body
+      }
+    }
+  })
+})
 
 new Vue({
   render: h => h(App),
